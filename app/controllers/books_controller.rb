725b2f3acb86063608ab685f8d03e294d3c15249
EC2 Default User <ec2-user@ip-172-31-82-237.ec2.internal>
2022-02-23 10:34:06 +0000

    [Add]bookindexページcreate

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+
+  def edit
+
+  end
+
+  def show
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+
+    redirect_to book_path(@book.id)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "6f34278fed454a3c81de53b7fed77db3.vfs.cloud9.us-east-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
